83cd0aa8f589cb86b5827e5ab0d293c271c9acdb
MobileID <123370216+vbmobile@users.noreply.github.com>
2026-04-16 12:44:59 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AMADocModeliOS",
-            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocModeliOS/AMADocModeliOS-1.0.0-rc24.zip",
+            url: "https://vbmobileidstorage.blob.core.windows.net/ios/AMADocModeliOS/AMADocModeliOS-1.0.0-rc23.zip",
             checksum: "a1e30e5b8c17180be35f14ecfa1831c3547f8e8ee44f95b3c6c22aaa4f1f4c3c"
         )
     ]
